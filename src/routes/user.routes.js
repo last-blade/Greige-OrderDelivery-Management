2c7b83ba26aab7a6f488e5bd1859b57b1ee69730
last-blade <meprashanttyagi2000@gmail.com>
2025-07-03 15:30:33 +0530

    feat: add logout route with authentication middleware

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,13 @@
 import Router from "express";
 import { loginUser } from "../controllers/userControllers/longinUser.controller.js";
 import { registerUser } from "../controllers/userControllers/registerUser.controller.js";
+import { logoutUser } from "../controllers/userControllers/logoutUser.controller.js";
+import { authentication } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/login").post(loginUser);
 router.route("/register").post(registerUser);
+router.route("/logout").post(authentication, logoutUser);
 
 export default router;
