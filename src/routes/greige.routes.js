601d116d4e6909c191ad888617a2badefd9f7325
last-blade <meprashanttyagi2000@gmail.com>
2025-07-03 16:48:03 +0530

    feat: add PUT route for editing Greige orders with authentication

--- src/routes/greige.routes.js
+++ src/routes/greige.routes.js
@@ -3,6 +3,7 @@ import { authentication } from "../middlewares/auth.middleware.js";
 import { authorizeRoles } from "../middlewares/authorizeRoles.middleware.js";
 import { createGreigeOrder } from "../controllers/greigeController/createGreigeOrder.controller.js";
 import { getGreigeOrders } from "../controllers/greigeController/getGreigeOrders.controller.js";
+import { editGreigeOrder } from "../controllers/greigeController/editGreigeOrder.controller.js";
 
 const router = Router();
 
@@ -11,5 +12,8 @@ router.route("/create-greige-order").post(authentication, authorizeRoles("Admin"
 
 //GET
 router.route("/greige-orders").get(authentication, getGreigeOrders);
+
+//PUT
+router.route("/edit-greige-order/:orderId").put(authentication, editGreigeOrder);
 
 export default router;
