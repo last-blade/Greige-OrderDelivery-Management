86d0fe77dc47298faf35ed4f241e67cda622ac36
last-blade <meprashanttyagi2000@gmail.com>
2025-07-03 16:28:57 +0530

    feat: add POST route for creating Unit3 orders with authentication and role authorization

--- src/routes/unit3.routes.js
+++ src/routes/unit3.routes.js
@@ -1,10 +1,12 @@
 import Router from "express";
 import { authentication } from "../middlewares/auth.middleware.js";
+import { createUnit3Order } from "../controllers/unit3Controllers/createUnit3Order.controller.js";
+import { authorizeRoles } from "../middlewares/authorizeRoles.middleware.js";
 
 const router = Router();
 
 //POST
-
+router.route("/create-unit3-order/:orderId").post(authentication, authorizeRoles("Admin", "UNIT3"), createUnit3Order);
 
 //GET
 
