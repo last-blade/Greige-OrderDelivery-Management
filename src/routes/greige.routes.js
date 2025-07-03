70dcb8b447994eccec6ade3db6f67d7a9a00733a
last-blade <meprashanttyagi2000@gmail.com>
2025-07-03 15:49:02 +0530

    feat: add GET route for fetching user greige orders

--- src/routes/greige.routes.js
+++ src/routes/greige.routes.js
@@ -2,9 +2,14 @@ import Router from "express";
 import { authentication } from "../middlewares/auth.middleware.js";
 import { authorizeRoles } from "../middlewares/authorizeRoles.middleware.js";
 import { createGreigeOrder } from "../controllers/greigeController/createGreigeOrder.controller.js";
+import { getGreigeOrders } from "../controllers/greigeController/getGreigeOrders.controller.js";
 
 const router = Router();
 
+//POST
 router.route("/create-greige-order").post(authentication, authorizeRoles("Admin", "GREIGE"), createGreigeOrder)
+
+//GET
+router.route("/greige-orders").get(authentication, getGreigeOrders);
 
 export default router;
