583440735d45dab044eba8838aa2c544debe9506
last-blade <meprashanttyagi2000@gmail.com>
2025-07-03 16:47:58 +0530

    feat: implement editGreigeOrder controller for updating Greige orders

---
+++ src/controllers/greigeController/editGreigeOrder.controller.js
@@ -0,0 +1,42 @@
+import { apiError, apiResponse, asyncHandler, Greige } from "../allImports.js";
+
+const editGreigeOrder = asyncHandler(async (request, response) => {
+    const {orderId} = request.params;
+    const {orderDate, orderNo, fabricName, requiredAmount, location, deliveryDate, remarks, recd, balance} = request.body;
+
+    if([orderDate, orderNo, fabricName, requiredAmount, location, deliveryDate].some(inputField => inputField === undefined || inputField.trim === "")){
+        throw new apiError(400, "All fields are required")
+    }
+
+    if(!orderId){
+        throw new apiError(400, "Order ID required")
+    }
+
+    const foundGreigeOrder = await Greige.findById(orderId);
+
+    if(!foundGreigeOrder){
+        throw new apiError(404, "Greige order not found")
+    }
+
+    const updatedGreigeOrder = await Greige.findByIdAndUpdate(orderId, {
+        $set: {
+            orderDate, 
+            orderNo, 
+            fabricName, 
+            requiredAmount, 
+            location, 
+            deliveryDate, 
+            remarks, 
+            recd, 
+            balance
+        }
+    }, {new: true}).populate("unit3OrderId").select("-greigeCreator -unit3Creator");
+
+    return response.status(200)
+    .json(
+        new apiResponse(200, updatedGreigeOrder, "Greige order updated")
+    )
+
+});
+
+export {editGreigeOrder}
