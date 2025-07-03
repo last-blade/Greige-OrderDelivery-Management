9e464c6c177005d9f34bb61f8063a9231845ed8f
last-blade <meprashanttyagi2000@gmail.com>
2025-07-03 13:55:33 +0530

    feat: implement registerUser controller for user registration

---
+++ src/controllers/userControllers/registerUser.controller.js
@@ -0,0 +1,30 @@
+import { apiError, apiResponse, asyncHandler, User } from "../allImports.js";
+
+const registerUser = asyncHandler(async (request, response) => {
+    const {fullName, email, accountType, passowrd} = request.body;
+
+    if([fullName, email, accountType, passowrd].some(inputField => inputField === undefined || inputField.trim() === "")){
+        throw new apiError(400, "All fields are required")
+    }
+
+    const foundUser = await User.find({email});
+
+    if(foundUser){
+        throw new apiError(409, "User with this email already exists")
+    }
+
+    await User.create({
+        fullName, 
+        email, 
+        accountType, 
+        passowrd
+    });
+
+    return response.status(201)
+    .json(
+        new apiResponse(201, {}, "User registered successfully")
+    )
+
+});
+
+export {registerUser}
