a6cb9e5a0119ed62ea5e0011a732b4ed00c24ed6
last-blade <meprashanttyagi2000@gmail.com>
2025-07-03 17:26:37 +0530

    feat: add PUT route for editing Unit3 orders with authentication and role authorization

--- src/routes/unit3.routes.js
+++ src/routes/unit3.routes.js
@@ -2,13 +2,15 @@ import Router from "express";
 import { authentication } from "../middlewares/auth.middleware.js";
 import { createUnit3Order } from "../controllers/unit3Controllers/createUnit3Order.controller.js";
 import { authorizeRoles } from "../middlewares/authorizeRoles.middleware.js";
+import { editUnit3Order } from "../controllers/unit3Controllers/editUnit3Order.controller.js";
 
 const router = Router();
 
 //POST
 router.route("/create-unit3-order/:orderId").post(authentication, authorizeRoles("Admin", "UNIT3"), createUnit3Order);
 
-//GET
+//PUT
+router.route("/edit-unit3-order/:orderId").put(authentication, authorizeRoles("Admin", "UNIT3"), editUnit3Order);
 
 
 export default router;
