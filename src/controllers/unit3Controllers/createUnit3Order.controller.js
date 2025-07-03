ce4d3e02dd75746f0b426dc7ac4d22982dfa3fe0
last-blade <meprashanttyagi2000@gmail.com>
2025-07-03 16:19:02 +0530

    feat: implement createUnit3Order controller for creating Unit3 orders

---
+++ src/controllers/unit3Controllers/createUnit3Order.controller.js
@@ -0,0 +1,40 @@
+import { apiError, asyncHandler, Greige, Unit3 } from "../allImports.js";
+
+const createUnit3Order = asyncHandler(async (request, response) => {
+    const {orderId} = request.params;
+
+    const {stock, plannedQuantity, estimatedDeliveryDate, revisedEstimatedDeliveryDate1, revisedEstimatedDeliveryDate2, days} = request.body;
+
+    if(!orderId){
+        throw new apiError(400, "Order ID is required")
+    }
+
+    if([stock, plannedQuantity, estimatedDeliveryDate, days].some(inputField => inputField === undefined || inputField.trim === "")){
+        throw new apiError(400, "All fields are required")
+    }
+
+    const foundGreigeOrder = await Greige.findById(orderId);
+
+    if(!foundGreigeOrder){
+        throw new apiError(404, "Greige order not found")
+    }
+
+    const createdUnit3Order = await Unit3.create({
+        stock, 
+        plannedQuantity, 
+        estimatedDeliveryDate, 
+        revisedEstimatedDeliveryDate1, 
+        revisedEstimatedDeliveryDate2, 
+        days,
+        greigeOrderId: foundGreigeOrder._id,
+    });
+
+    foundGreigeOrder.unit3OrderId = foundGreigeOrder._id;
+    foundGreigeOrder.save({validateBeforeSave: false});
+
+    const foundUnit3Order = await Unit3.findById(createdUnit3Order._id).populate("greigeOrderId").select("-greigeCreator -unit3Creator");
+    
+
+});
+
+export {createUnit3Order}
