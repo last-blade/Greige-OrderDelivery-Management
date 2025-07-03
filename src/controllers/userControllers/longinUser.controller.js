918d87cfcbf4fd23ccc3dbeb903114ffc05bd3eb
last-blade <meprashanttyagi2000@gmail.com>
2025-07-03 13:55:28 +0530

    feat: implement loginUser controller for user authentication

---
+++ src/controllers/userControllers/longinUser.controller.js
@@ -0,0 +1,40 @@
+import { accessTokenCookieOptions, refreshTokenCookieOptions } from "../../../constants.js";
+import { apiError, apiResponse, asyncHandler, generateAccessToken, generateRefreshToken, User } from "../allImports.js";
+
+const loginUser = asyncHandler(async (request, response) => {
+    const {email, password} = request.body;
+
+    if(!email.trim()){
+        throw new apiError(400, "Email is required")
+    }
+
+    if(!password){
+        throw new apiError(400, "Password is required")
+    }
+
+    const foundUser = await User.find({email});
+
+    if(!foundUser){
+        throw new apiError(404, "User not found")
+    }
+
+    const accessToken = await generateAccessToken(foundUser._id);
+    const refreshToken = await generateRefreshToken(foundUser._id);
+
+    if(!accessToken || !refreshToken){
+        throw new apiError(500, "Something went wrong while generating tokens")
+    }
+
+    foundUser.refreshToken = refreshToken;
+    foundUser.save({validateBeforeSave: false});
+
+    return response.status(200)
+    .cookie("accessToken", accessToken, accessTokenCookieOptions)
+    .cookie("refreshToken", refreshToken, refreshTokenCookieOptions)
+    .json(
+        new apiResponse(200, foundUser, "Login successfully")
+    )
+    
+});
+
+export {loginUser}
