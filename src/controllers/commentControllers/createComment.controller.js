518722de5c2ed20ecf7bf490d6534ddca463f1f1
last-blade <meprashanttyagi2000@gmail.com>
2025-07-04 16:27:56 +0530

    Add create comment functionality

---
+++ src/controllers/commentControllers/createComment.controller.js
@@ -0,0 +1,30 @@
+import { apiResponse, asyncHandler, Comment } from "../allImports.js";
+
+const createComment = asyncHandler(async (request, response) => {
+    const {orderId} = request.params;
+    const {comment} = request.body;
+
+    if(!orderId){
+        throw new apiError(400, "Order ID not found")
+    }
+
+    const foundGreigeOrder = await Greige.findById(orderId);
+
+    if(!foundGreigeOrder){
+        throw new apiError(404, "Greige order not found")
+    }
+
+    const createdComment = await Comment.create({
+        comment,
+        commentor: request.user.id,
+        commentedOrder: orderId,
+    });
+
+    return response.status(201)
+    .json(
+        new apiResponse(201, createdComment, `Commented on: ${foundGreigeOrder.orderNo}`)
+    )
+
+});
+
+export {createComment}
