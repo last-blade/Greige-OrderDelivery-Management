59e76da97e146d4603142cab6ca4d238843ed7f1
last-blade <meprashanttyagi2000@gmail.com>
2025-07-04 11:18:36 +0530

    feat: send email notification upon greige order creation

--- src/controllers/greigeController/createGreigeOrder.controller.js
+++ src/controllers/greigeController/createGreigeOrder.controller.js
@@ -1,3 +1,4 @@
+import greigeOrderPlacedEmail from "../../emails/greigeOrderPlacedEmail.js";
 import { apiError, apiResponse, asyncHandler, Greige } from "../allImports.js";
 
 const createGreigeOrder = asyncHandler(async (request, response) => {
@@ -25,6 +26,20 @@ const createGreigeOrder = asyncHandler(async (request, response) => {
     if(!foundGreigeOrder){
         throw new apiError(500, "Something went wrong while creating greige order")
     }
+
+    await greigeOrderPlacedEmail({
+        recipientName: "UNIT3",
+        recipientEmail: "sunny@natharts.com",
+        orderDate: foundGreigeOrder.orderDate,
+        orderNo: foundGreigeOrder.orderNo,
+        fabricName: foundGreigeOrder.fabricName,
+        requiredAmount: foundGreigeOrder.requiredAmount,
+        location: foundGreigeOrder.location,
+        deliveryDate: foundGreigeOrder.deliveryDate,
+        remarks: foundGreigeOrder.remarks,
+        recd: foundGreigeOrder.recd,
+        balance: foundGreigeOrder.balance,
+    });
 
     return response.status(201)
     .json(
