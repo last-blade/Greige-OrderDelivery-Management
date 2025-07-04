128a7d519bbf18d376fcb6f8beb73f4c78d881fd
last-blade <meprashanttyagi2000@gmail.com>
2025-07-04 13:35:47 +0530

    feat: implement delete functionality for greige orders

---
+++ src/controllers/greigeController/deleteGreigeOrder.controller.js
@@ -0,0 +1,28 @@
+import { apiError, apiResponse, asyncHandler, Greige, Unit3 } from "../allImports.js";
+
+const deleteGreigeOrder = asyncHandler(async (request, response) =>{
+    const {orderId} = request.params;
+
+    if(!orderId){
+        throw new apiError(400, "Greige order ID is required")
+    }
+
+    const foundGreigeOrder = await Greige(orderId);
+
+    if(!foundGreigeOrder){
+        throw new apiError(404, "Greige order not found maybe already deleted")
+    }
+
+    const unit3OrderId = foundGreigeOrder.unit3OrderId;
+
+    await Unit3.findByIdAndDelete(unit3OrderId);
+    await Greige.findByIdAndDelete(orderId);
+
+    return response.status(200)
+    .json(
+        new apiResponse(200, {}, "Greige order deleted")
+    )
+
+});
+
+export {deleteGreigeOrder}
