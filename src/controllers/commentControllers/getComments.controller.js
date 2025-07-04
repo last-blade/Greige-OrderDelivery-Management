d76853f1b6a9600db3da318648095d8fe96091d3
last-blade <meprashanttyagi2000@gmail.com>
2025-07-04 16:28:18 +0530

    Add fetch comments of particular Greige Order

---
+++ src/controllers/commentControllers/getComments.controller.js
@@ -0,0 +1,27 @@
+import { apiResponse, asyncHandler, Comment } from "../allImports.js";
+
+const getComments = asyncHandler(async (request, response) => {
+    const {orderId} = request.params;
+
+    if(!orderId){
+        throw new apiError(400, "Order ID not found")
+    }
+
+    const foundGreigeOrder = await Greige.findById(orderId);
+
+    if(!foundGreigeOrder){
+        throw new apiError(404, "Greige order not found")
+    }
+
+    const comments = await Comment.find({
+        commentedOrder: orderId,
+    });
+
+    return response.status(200)
+    .json(
+        new apiResponse(200, comments, "Comments fetched")
+    )
+
+});
+
+export {getComments}
